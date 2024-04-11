638522dbe852e4033bad4db5157eb2078b087289
samuel <samuelpc4567@gmail.com>
2024-04-10 22:00:08 -0300

    Implementação dos Algoritmos de Ordenação

---
+++ main.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Bubble Sort
+void bubbleSort(vector<int> &v) {
+    int n = v.size();
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(v[j] > v[j + 1]) {
+                int temp = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Quick Sort
+int partition(vector<int> &arr, int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+void quickSort(vector<int> &arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+// Insertion Sort
+void insertionSort(vector<int> &v) {
+    int n = v.size();
+    for (int i = 1; i < n; ++i) {
+        int key = v[i];
+        int j = i - 1;
+
+        while (j >= 0 && v[j] > key) {
+            v[j + 1] = v[j];
+            j = j - 1;
+        }
+        v[j + 1] = key;
+    }
+}
+
+// Merge Sort
+void merge(vector<int> &v, int low, int high, int mid) {
+    int i, j, k;
+    vector<int> c(high - low + 1); // vetor temporário dinâmico
+
+    i = low;
+    k = 0;
+    j = mid + 1;
+    while (i <= mid && j <= high) {
+        if (v[i] < v[j]) {
+            c[k] = v[i];
+            k++;
+            i++;
+        } else {
+            c[k] = v[j];
+            k++;
+            j++;
+        }
+    }
+    while (i <= mid) {
+        c[k] = v[i];
+        k++;
+        i++;
+    }
+    while (j <= high) {
+        c[k] = v[j];
+        k++;
+        j++;
+    }
+    for (i = low, k = 0; i <= high; i++, k++) {
+        v[i] = c[k];
+    }
+}
+
+void mergeSort(vector<int> &v, int low, int high) {
+    if(low < high) {
+        int mid = (low + high) / 2;
+        mergeSort(v, low, mid);
+        mergeSort(v, mid + 1, high);
+        merge(v, low, high, mid);
+    }
+}
+
+// Shell Sort
+void shellSort(vector<int> &v) {
+    int n = v.size();
+    for (int h = n / 2; h > 0; h /= 2) {
+        for (int i = h; i < n; i += 1) {
+            int temp = v[i];
+            int j;
+            for (j = i; j >= h && v[j - h] > temp; j -= h)
+                v[j] = v[j - h];
+            v[j] = temp;
+        }
+    }
+}
+
+// Selection Sort
+void selectionSort(vector<int> &v) {
+    int min_idx;
+    int n = v.size();
+    for(int i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for(int j = i + 1; j < n; j++) {
+            if(v[j] < v[min_idx])
+                min_idx = j;
+        }
+        if(min_idx != i) {
+            int temp = v[min_idx];
+            v[min_idx] = v[i];
+            v[i] = temp;
+        }
+    }
+}
+main(){
+    return 0;
+}
